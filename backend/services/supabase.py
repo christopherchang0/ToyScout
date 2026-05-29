570c766666ae6e2d51e45844e3f625d2f4e99ba1
Christopher Chang <christopherchangg0@gmail.com>
2026-05-29 11:39:15 -0700

    load .env

--- backend/services/supabase.py
+++ backend/services/supabase.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+from supabase import Client, create_client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_KEY")
+key: str = os.environ.get("SUPABASE_URL")
+supabase: Client = create_client(url, key)
 
 def save_scan():
     return 
