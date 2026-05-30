6e156eecf936eb8549bf1143d73e9b230fa6a75b
Christopher Chang <christopherchangg0@gmail.com>
2026-05-30 16:17:00 -0700

    call firebase to each /scan holder stage

--- backend/routes/scan.py
+++ backend/routes/scan.py
@@ -4,15 +4,20 @@ import tempfile
 from fastapi import APIRouter, File, UploadFile
 from services.vision import imagescanner
 from services.supabase import save_scan
+from services.firebase import set_status
+import uuid
 
 router = APIRouter()
 
 @router.post("/scan")
 async def scan_file(file: UploadFile = File(...)):
     #read the file content. waits until it is read
+    scan_id = str(uuid.uuid4())
+    set_status(scan_id, "Uploading")
     content = await file.read() #reads file into bytes
 
     #scanning logic
+    set_status(scan_id, "Analyzing")
     with tempfile.NamedTemporaryFile(delete=False, suffix = "png") as tmp:
         tmp.write(content) #writes bytes to an actual file on disk
         tmp_path = tmp.name #saves the file path
@@ -23,7 +28,8 @@ async def scan_file(file: UploadFile = File(...)):
     finally:
         os.remove(tmp_path)
 
-    save_scan(toy_info)
+    set_status(scan_id, "Done")
+    save_scan({**toy_info, "id": scan_id})
 
     return toy_info
 
