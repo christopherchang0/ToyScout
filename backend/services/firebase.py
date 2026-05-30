ded840189ff9a5907129e89f6636c556b43356bd
Christopher Chang <christopherchangg0@gmail.com>
2026-05-30 16:10:41 -0700

    added firebase credentials

--- backend/services/firebase.py
+++ backend/services/firebase.py
@@ -1,6 +1,15 @@
 import os
+import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import credentials, db
+
+load_dotenv()
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(os.environ.get("FIREBASE_CREDENTIALS_PATH"))
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": os.environ.get("FIREBASE_DATABASE_URL")
+    })
 
 def set_status(scan_id: str, status: str):
     #Reference to specific scan mode
