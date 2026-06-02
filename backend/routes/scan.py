6b29fb014cfa697edcd642f03d33356a6040be90
Christopher Chang <christopherchangg0@gmail.com>
2026-06-02 13:23:23 -0700

    readable comments

--- backend/routes/scan.py
+++ backend/routes/scan.py
@@ -11,17 +11,17 @@ router = APIRouter()
 
 @router.post("/scan")
 async def scan_file(file: UploadFile = File(...)):
-    #read the file content. waits until it is read
+    #Read the file content. waits until it is read
     scan_id = str(uuid.uuid4())
     set_status(scan_id, "Uploading")
     content = await file.read() #reads file into bytes
 
-    #scanning logic
+    #Scanning logic
     set_status(scan_id, "Analyzing")
     with tempfile.NamedTemporaryFile(delete=False, suffix = "png") as tmp:
         tmp.write(content) #writes bytes to an actual file on disk
         tmp_path = tmp.name #saves the file path
-    #ensures temp file gets deleted even if imagescanner has error
+    #Ensures temp file gets deleted even if imagescanner has error
     try:
         result = imagescanner(tmp_path, "Identify this toy.")
         # Strip markdown code blocks if present
